478e0ba975c9659dd2ae31604c2faf300b82e964
Christian Jabasa <cljabasa@gmail.com>
2021-07-25 10:10:40 +0800

    work sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,22 @@
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-  // more options here
+  theme: {
+    fontFamily: {
+      sans: [
+        'Work Sans',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Helvetica',
+        'Arial',
+        'Roboto',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+      ],
+      mono: ['Menlo', 'Consolas', 'monospaced'],
+    },
+  },
 };
